54c6614cc453fd2f1d77aeb53061a4bdcebb05c3
Elizabeth Reynolds <e.d.reynolds@icloud.com>
2026-03-11 17:57:58 -0500

    added footer

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <>Footer</>;
+  return (
+    <>
+      <footer class="footer mt-auto py-3 bg-dark">
+        <div class="container">
+          <span class="text-white">Copyright © your website 2026</span>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
